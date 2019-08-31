e0f1f5f79e3f06a7a633bcc05dbaa052a82de6e9
Sarah Wise <skjwise@hotmail.com>
2019-08-31 12:27:06 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,14 +1,4 @@
-def consolidate_cart(cart:[])
-  cosolidated_cart = {}
-  cart.each do |item|
-    item_name = item.key[0]
-    cosolidated_cart[item_name] = item_values[0]
-    if cosolidated_cart[item_name][:count]
-      cosolidated_cart[item_name][:count] += 1
-    else
-      cosolidated_cart[item_name][:count] = 1
-    end
-  end
+def consolidate_cart(cart)
   cosolidated_cart
 end
 

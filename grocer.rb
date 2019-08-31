d4d08f8f5873656beb339b4223491f5ac859a72e
Sarah Wise <skjwise@hotmail.com>
2019-08-31 13:08:23 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,5 +1,8 @@
 def consolidate_cart(cart)
-  
+  new_cart = {}
+  cart.each do |items_array|
+    items_array.each do |item, attribute_hash|
+      new_cart[item] 
 end
 
 def apply_coupons(cart, coupons)

f79b20534070a0e69c84c26680dbcbfcde640bd6
Sarah Wise <skjwise@hotmail.com>
2019-08-31 12:40:06 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -18,8 +18,10 @@ def apply_coupons(cart, coupons)
           cart["#{name} W/COUPON"][:count] += 1
         else
           cart["#{name} W/COUPON"] = {
-            
+            :price => coupon[:cost], :clearance => cart[name][:clearance],
+            :count => 1
           }
+          
 end
 
 def apply_clearance(cart)

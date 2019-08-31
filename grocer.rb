1a8f68d0c540dba95716576f17334576c997efa6
Sarah Wise <skjwise@hotmail.com>
2019-08-31 12:56:26 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -19,7 +19,7 @@ def apply_coupons(cart, coupons)
     name_of_item = item[:item]
     if cart.has_key?(name_of_item) == true && cart[name_of_item][:count] >= item[:num]
       cart[name_of_item][:count] = cart[name_of_item][:count] - item[:num]
-      new_item = new_item + ("W/COUPON")
+      new_item = new_item + (" W/COUPON")
 end
 
 def apply_clearance(cart)
